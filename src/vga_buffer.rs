56fcdebf2c55bbe72f16b284516cd3001961f4fa
Mysti Quant <mystiquant@icloud.com>
2024-09-03 11:32:19 -0700

    Created a Test Framework for Rust Kernel
    I used Rust's custom test framework to
    make support for the #[test_case] feature.
    QEMU ui is no longer triggered. A basic
    driver for serial port was made to support
    QEMU to report the test status into console
    rather than VGA buffer. Created a tests/
    directory and ported basic boot tests as
    a proof of concept. Boilerplate code to run
    tests was written into a library (lib.rs).

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -148,3 +148,29 @@ pub fn _print(args: fmt::Arguments) {
     use core::fmt::Write;
     WRITER.lock().write_fmt(args).unwrap();
 }
+
+// tests that one line can quickly print
+#[test_case]
+fn test_println_simple() {
+    println!("test_println_simple output");
+}
+
+// tests that no panic occurs even though many lines printed
+#[test_case]
+fn test_println_many() {
+    for _ in 0..200 {
+        println!("test_println_many output");
+    }
+}
+
+// tests if what is printed is actually whats read on the line
+#[test_case]
+fn test_println_output() {
+    let s = "Some test string that fits on a single line";
+
+    println!("{}", s);
+    for (i, c) in s.chars().enumerate() {
+        let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT -2][i].read();
+        assert_eq!(char::from(screen_char.ascii_character), c)
+    }
+}
