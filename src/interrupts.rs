f3a8d5528b159cc5856671abd9af9a543dd27fa9
Mysti Quant <mystiquant@icloud.com>
2024-09-04 11:26:52 -0700

    Properly Handling Double Faults
    There are several ways double faults can
    occur, roughly 25 which I've learned about.
    Using an Interrupt Stack Table (ISS) and the
    archaic Task State Segment (TSS)—with a fixed
    pre allocated stack—to allow switching
    of stack frames in case of DOUBLE FAULTS. In order
    to use the TSS, I used the `x86_64` crate to create
    a Global Descriptor Table. Then I wrote a double fault
    test—a function loops infinitely, causing the program
    to use all all stack space, resulting in stack overflow
    and the result should be TSS switches to the double
    fault stack and continue execution of the OS.

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,5 +1,6 @@
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 use crate::println;
+use crate::gdt;
 use lazy_static::lazy_static;
 
 // Using lazy static here, because we need the
@@ -12,6 +13,11 @@ lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(breakpoint_handler);
+        unsafe {
+            idt.double_fault.set_handler_fn(double_fault_handler)
+            .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
+        }
+
         idt
     };
 }
@@ -27,7 +33,9 @@ extern "x86-interrupt" fn breakpoint_handler(
     println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
 }
 
-#[test_case]
-fn test_breakpoint_exception() {
-    x86_64::instructions::interrupts::int3();
+extern "x86-interrupt" fn double_fault_handler(
+    stack_frame: InterruptStackFrame,
+    _error_code: u64
+) -> ! {
+    panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
 }
